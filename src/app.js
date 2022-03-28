19a048aa09e79593c01ed92ab38e106f0c434045
Olomukoro Alexander <swankylex@gmail.com>
2022-03-28 14:24:47 +0800

    Adjusted my name to uppercase

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ app.use(express.static(publicDirectoryPath));
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather App",
-    name: "olomukoro Alexander"
+    name: "Olomukoro Alexander"
   });
 });
 
